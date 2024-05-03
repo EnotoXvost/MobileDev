88952c535f4c7c12bb9109860e56d67110b9d65a
EnotoXvost <a.i.1003@mail.ru>
2024-05-03 15:13:08 +0300

    lesson 5

---
+++ MireaProject/app/src/main/java/ru/mirea/afanaseviv/mireaproject/CompassFragment.java
@@ -0,0 +1,83 @@
+package ru.mirea.afanaseviv.mireaproject;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class CompassFragment extends Fragment implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor accelerometerSensor, magneticFieldSensor;
+    private final float[] lastAccelerometer = new float[3];
+    private final float[] lastMagnetometer = new float[3];
+    private final float[] rotationMatrix = new float[9];
+    private final float[] orientationAngles = new float[3];
+    private boolean isAccelerometerSet = false;
+    private boolean isMagnetometerSet = false;
+    private TextView directionTextView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        sensorManager = (SensorManager) requireActivity().getSystemService(Context.SENSOR_SERVICE);
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magneticFieldSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_compass, container, false);
+        directionTextView = view.findViewById(R.id.text_direction);
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (accelerometerSensor != null) {
+            sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (magneticFieldSensor != null) {
+            sensorManager.registerListener(this, magneticFieldSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, lastAccelerometer, 0, event.values.length);
+            isAccelerometerSet = true;
+        } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, lastMagnetometer, 0, event.values.length);
+            isMagnetometerSet = true;
+        }
+        if (isAccelerometerSet && isMagnetometerSet) {
+            SensorManager.getRotationMatrix(rotationMatrix, null, lastAccelerometer, lastMagnetometer);
+            SensorManager.getOrientation(rotationMatrix, orientationAngles);
+            float azimuthInRadians = orientationAngles[0];
+            float azimuthInDeg = (float) (Math.toDegrees(azimuthInRadians) + 360) % 360;
+            directionTextView.setText(String.format("North Direction: %.2f°", azimuthInDeg));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy){
+    }
+}
